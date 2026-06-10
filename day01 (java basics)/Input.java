036abf337f5a8675f680f31a2a1c43d561599b8a
ridhimsharma-val <ridhimsharma38@gmail.com>
2026-06-11 02:20:42 +0530

    Delete day01 (java basics) directory

--- day01 (java basics)/Input.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Input {
-    public static void main(String[] args){
-        Scanner input =new Scanner(System.in);
-        System.out.println(input.nextInt());
-        input.close();
-    }
-}
